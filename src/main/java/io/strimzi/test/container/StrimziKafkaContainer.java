a1d14d611f3dd7926b45a707fe5b52293e9f5760
morsak <xorsak02@stud.fit.vutbr.cz>
2021-12-15 18:21:31 +0100

    import
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- src/main/java/io/strimzi/test/container/StrimziKafkaContainer.java
+++ src/main/java/io/strimzi/test/container/StrimziKafkaContainer.java
@@ -24,7 +24,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * StrimziKafkaContainer is a single-node instance of Kafka using the image from quay.io/strimzi/kafka with the
