53e031ed22b0514e310703fe2bee3afa633ef17f
see-quick <maros.orsak159@gmail.com>
2023-11-30 11:35:09 +0100

    Add Toxiproxy client + Proxy remove deprecated stuff
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- src/main/java/io/strimzi/test/container/StrimziKafkaCluster.java
+++ src/main/java/io/strimzi/test/container/StrimziKafkaCluster.java
@@ -4,6 +4,7 @@
  */
 package io.strimzi.test.container;
 
+import eu.rekawek.toxiproxy.ToxiproxyClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Container;
